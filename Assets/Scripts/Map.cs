011e05a6c96122632ac478e5975426b0ebdd6325
Hugo Scurti <hugoscurti@gmail.com>
2017-03-31 21:29:57 -0400

    Merge LoadMap into Map Class; Change MapLoader to MapDisplay

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -1,4 +1,7 @@
-﻿
+﻿using System;
+using System.IO;
+using UnityEngine;
+
 
 public class Map
 {
@@ -10,6 +13,9 @@ public class Map
     //Consider storing obstacles in a Hashset to save memory on large maps
     public bool[][] Obstacles { get; set; }
 
+    //Original characters that forms the whole map
+    public char[][] Tiles { get; set; }
+
     public Map() {}
 
     public Map(int Width, int Height, bool[][] Obstacles, int FreeTiles)
@@ -19,4 +25,117 @@ public class Map
         this.Obstacles = Obstacles;
         this.FreeTiles = FreeTiles;
     }
+
+    /// <summary>
+    /// Loads a map from the base map directory
+    /// </summary>
+    /// <param name="MapName">File from which to load the map</param>
+    public static Map LoadMap(string MapName)
+    {
+        string BaseMapDirectory = Path.Combine(Application.dataPath, "../Maps/map");
+        FileInfo f = new FileInfo(Path.Combine(BaseMapDirectory, MapName));
+
+        return ReadMap(f);
+    }
+
+    /// <summary>
+    /// Reads map and returns a map object 
+    /// </summary>
+    static Map ReadMap(FileInfo file)
+    {
+        Map map = new Map();
+
+        using (FileStream fs = file.OpenRead())
+        using (StreamReader sr = new StreamReader(fs))
+        {
+
+            //Line 1 : type octile
+            ReadLine(sr, "type octile");
+
+            //Line 2 : height
+            map.Height = ReadIntegerValue(sr, "height");
+
+            //Line 3 : width
+            map.Width = ReadIntegerValue(sr, "width");
+
+            //Line 4 to end : map
+            ReadLine(sr, "map");
+
+            map.Obstacles = new bool[map.Height][];
+            map.FreeTiles = 0;
+
+            //Store the array of characters that makes up the map
+            map.Tiles = new char[map.Height][];
+
+            //Read tiles section
+            ReadTiles(sr, map);
+
+            return map;
+        }
+    }
+
+    /// <summary>
+    /// Read a line and expect the line to be the value passed in arguments
+    /// </summary>
+    private static void ReadLine(StreamReader sr, string value)
+    {
+        string line = sr.ReadLine();
+        if (line != value) throw new Exception(
+                string.Format("Invalid format. Expected: {0}, Actual: {1}", value, line));
+    }
+
+    /// <summary>
+    /// Returns an integer value from the streamreader that comes
+    /// right after a key separated by a space.
+    /// I.E. width 5
+    /// </summary>
+    private static int ReadIntegerValue(StreamReader sr, string key)
+    {
+        string[] block = sr.ReadLine().Split(' ');
+        if(block[0] != key) throw new Exception(
+                string.Format("Invalid format. Expected: {0}, Actual: {1}", key, block[0]));
+
+        return int.Parse(block[1]);
+    }
+
+    /// <summary>
+    /// Read tiles from the map file, adding tiles and filling obstacles in the array
+    /// </summary>
+    static void ReadTiles(StreamReader sr, Map map)
+    {
+        char c;
+        string line;
+
+        for (int i = 0; i < map.Height; ++i)
+        {
+            line = sr.ReadLine();
+            map.Obstacles[i] = new bool[map.Width];
+            map.Tiles[i] = new char[map.Width];
+
+            for (int j = 0; j < map.Width; ++j)
+            {
+                c = line[j];
+                map.Tiles[i][j] = c;
+
+                switch (c)
+                {
+                    case '@':
+                    case 'O':
+                        map.Obstacles[i][j] = true;
+                        break;
+                    case 'T':
+                        map.Obstacles[i][j] = true;
+                        break;
+                    case '.':
+                    case 'G':
+                        map.Obstacles[i][j] = false;
+                        map.FreeTiles++;
+                        break;
+                    default:
+                        throw new Exception("Character not recognized");
+                }
+            }
+        }
+    }
+
 }
---
+++ Assets/Scripts/MapDisplay.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.IO;
+using System;
+
+public class MapLoader : MonoBehaviour {
+
+    //Variables to draw on gui
+    private Texture2D RectTexture;
+    private GUIStyle RectStyle;
+
+    private string BaseMapDirectory;
+
+    //Tile colors
+    public Color Green = Color.green;
+    public Color Black = Color.black;
+    public Color White = Color.white;
+
+    public RectTransform mainPanel;
+
+    private Map map;
+
+    //Tile size for drawing the map on screen
+    float TileUnit;
+
+    private void Awake()
+    {
+        BaseMapDirectory = Path.Combine(Application.dataPath, "../Maps/map");
+
+        map = Map.LoadMap("arena.map");
+    }
+   
+
+    private void OnGUI()
+    {
+        GUIDrawMap();
+    }
+
+    public void GUIDrawMap()
+    {
+        //TODO: Set size of a tile with respect to the map's size and the screen's ratio
+        if (map.Width > map.Height)
+            TileUnit = Screen.width / map.Width;
+        else
+            TileUnit = Screen.height / map.Height;
+
+        Rect tile = new Rect();
+        tile.width = TileUnit;
+        tile.height = TileUnit;
+
+        int startx = Mathf.FloorToInt((float)Screen.width / 2 - ((float)map.Width / 2) * TileUnit);
+        int starty = Mathf.FloorToInt((float)Screen.height / 2 - ((float)map.Height / 2) * TileUnit);
+
+        for (int i = 0; i < map.Tiles.Length; ++i)
+        {
+            for (int j = 0; j < map.Tiles[i].Length; ++j)
+            {
+                tile.x = j * TileUnit + startx;
+                tile.y = i * TileUnit + starty;
+
+                switch (map.Tiles[i][j])
+                {
+                    case '@':
+                    case 'O':
+                        GUIDrawRect(tile, Black);
+                        break;
+                    case 'T':
+                        GUIDrawRect(tile, Green);
+                        break;
+                    case '.':
+                    case 'G':
+                        GUIDrawRect(tile, White);
+                        break;
+                }
+            }
+        }
+    }
+
+    // Note that this function is meant to be called from OnGUI() functions.
+    public void GUIDrawRect(Rect position, Color color)
+    {
+        if (RectTexture == null)
+            RectTexture = new Texture2D(1, 1);
+
+        if (RectStyle == null)
+            RectStyle = new GUIStyle();
+
+        RectTexture.SetPixel(0, 0, color);
+        RectTexture.Apply();
+
+        RectStyle.normal.background = RectTexture;
+        
+        GUI.Box(position, GUIContent.none, RectStyle);
+    }
+}
