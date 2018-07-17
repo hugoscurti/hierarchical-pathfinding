c7d5e65b8c5a326499b70a1b9a13e357561d3fe3
Hugo Scurti <hugoscurti@gmail.com>
2018-07-17 15:23:12 -0400

    Renamed folder GraphUtilities to Graph; Added DisplayUtils static class;

---
+++ Assets/Scripts/Utilities/DisplayUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public static class DisplayUtils
+{
+    /// <summary>
+    /// Determine wether the mouse position is on the game's screen or not
+    /// </summary>
+    public static bool IsMouseOnScreen()
+    {
+        var mousePos = Input.mousePosition;
+        return mousePos.x > 0 &&
+            mousePos.y > 0 &&
+            mousePos.x <= Screen.width &&
+            mousePos.y <= Screen.height;
+    }
+
+}
